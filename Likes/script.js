f344961e6c902a5924ac1d5c0d6dddb0ef96bfcf
Sarah <116047642+ellaeslaflaca@users.noreply.github.com>
2022-11-13 15:18:04 -0700

    Add files via upload

---
+++ Likes/script.js
@@ -0,0 +1,35 @@
+// comments of erroneous code not removed for own reference
+// var counter1 = 0;
+var counter1 = document.querySelector("#counter1");
+
+function Add1(counter1){
+    // counter1++;
+    // counter1.innerText = counter1;
+    // console.log(counter1)
+    counter1.innerText++;
+    console.log(counter1);
+
+}
+
+
+
+// var counter2 = 0;
+var counter2 = document.querySelector("#counter2");
+
+function AddOne(counter2){
+    // counter2++;
+    // counter2.innerText = counter2;
+    // console.log(counter2)
+    counter2.innerText++;
+    console.log(counter2);
+}
+
+
+
+// var counter3 = 0;
+var counter3 = document.querySelector("#counter3");
+
+function AddOneMore(counter3){
+    counter3.innerText++;
+    console.log(counter3);
+}
